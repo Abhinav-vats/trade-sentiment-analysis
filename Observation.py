bf374929282b5f739977ac4b7be301a0ac77c5c8
Abhinav-vats <50848068+Abhinav-vats@users.noreply.github.com>
2025-01-06 23:23:53 +0530

    observation updates

--- Observation.py
+++ Observation.py
@@ -4,18 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-# Sample dataset
-# data = {
-#     "text": [
-#         "The market is booming with new opportunities.",
-#         "Investors are worried about potential losses.",
-#         "Stock prices are stable for now.",
-#         "A major crash in the market is expected.",
-#         "The economy is showing positive growth."
-#     ],
-#     "sentiment_score": [0.8, -0.7, 0.1, -0.9, 0.9]
-# }
-
 # Create a DataFrame
 df = pd.read_csv('data/Sentiment_analysis_Data.csv')
 print(df.columns)
@@ -45,7 +33,7 @@ print(f"Mean Squared Error: {mse}")
 print(f"R^2 Score: {r2}")
 
 # Test with a new sentence
-new_text = ["The market is uncertain with mixed signals."]
+new_text = ["The market is going not very good due to Ratan Tata's Death days."]
 new_features = vectorizer.transform(new_text)
 predicted_score = model.predict(new_features)
 print(f"Predicted Sentiment Score: {predicted_score[0]:.2f}")
